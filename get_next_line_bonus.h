383e9910d504d57ea172970f2ec22ee9b8e710bb
ejawe <doriane.montgenie@gmail.com>
2020-02-13 19:26:47 +0100

    gnl

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontgen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/03 15:47:45 by dmontgen          #+#    #+#             */
+/*   Updated: 2020/01/03 15:50:16 by dmontgen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontgen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/26 15:17:00 by dmontgen          #+#    #+#             */
+/*   Updated: 2020/01/08 12:44:01 by dmontgen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = -1;
+	if (!s1 || !s2)
+		return (0);
+	if (!(s3 = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (0);
+	while (s1[i])
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2[++j])
+	{
+		s3[i] = s2[j];
+		i++;
+	}
+	s3[i] = '\0';
+	return (s3);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	if (!(res = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	if (len == 0 || start > ft_strlen(s))
+	{
+		res[0] = 0;
+		return (res);
+	}
+	while (len--)
+		res[i++] = s[start++];
+	res[i] = 0;
+	return (res);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*new_string;
+
+	i = 0;
+	if (!(new_string = malloc(sizeof(char) * (ft_strlen(s) + 1))))
+		return (0);
+	while (s[i])
+	{
+		new_string[i] = s[i];
+		i++;
+	}
+	new_string[i] = '\0';
+	return (new_string);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
